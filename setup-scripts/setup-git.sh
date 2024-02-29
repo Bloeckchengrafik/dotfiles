fed63435384aa782a52e8ea89dfb2d00ad11cba2
Christian Bergschneider <christian.bergschneider@gmx.de>
2024-02-29 15:44:02 +0000

    feat: i3 & rofi

--- setup-scripts/setup-git.sh
+++ setup-scripts/setup-git.sh
@@ -4,9 +4,14 @@ set -e
 # Pull in my public key
 curl https://bloeckchengrafik.de/keys/ChristianBergschneider_0x5FA28D3E_public.asc | gpg --import
 
+# Fetch keys from smartcard
+echo "[!!!] Use the fetch and quit command here"
+gpg --edit-card 
+
 # Configure git client
 git config --global init.defaultBranch main
 git config --global user.signingkey F2212F875BB389BB762172E23991F97F5FA28D3E
 git config --global user.name "Christian Bergschneider"
 git config --global user.email "christian.bergschneider@gmx.de"
+git config --global core.editor "nvim"
 git config --global commit.gpgsign true 
